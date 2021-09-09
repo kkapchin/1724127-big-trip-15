c1a44e798bfd5db5b8bb913a1f545b6d26c60705
kkapchin <kkapchin@yandex.ru>
2021-09-09 18:26:33 +0300

    Рефакторинг utils.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import dayjs from 'dayjs';
 import { getPoints } from './mock/trips.js';
-import { calculateDuration } from './utils/calculate-duration.js';
-import { render } from './utils/render.js';
+import { getRouteInfo, renderTemplate } from './utils.js';
 import { createAppFiltersTemplate } from './view/app-filters.js';
 import { createAppMenuTemplate } from './view/app-menu.js';
 import { createAppSortTemplate } from './view/app-sort.js';
@@ -13,30 +11,11 @@ const appHeaderElement = appMainElement.querySelector('.trip-main');
 const appNavigationElement = appHeaderElement.querySelector('.trip-controls__navigation');
 const appFiltersElement = appHeaderElement.querySelector('.trip-controls__filters');
 const appEventsElement = appMainElement.querySelector('.trip-events');
-const data = getPoints(15);
-const points = data.map((element) => (
-  {
-    price: element.basePrice,
-    dispatchDate: dayjs(element.dateFrom).format('DD/MM/YY'),
-    dispatchTime: dayjs(element.dateFrom).format('HH:mm'),
-    arrivalDate: dayjs(element.dateTo).format('DD/MM/YY'),
-    arrivalTime: dayjs(element.dateTo).format('HH:mm'),
-    eventDay: dayjs(element.dateTo).format('MMM DD'),
-    dateClass: dayjs(element.dateFrom).format('YYYY-MM-DD'),
-    dispatchTimeClass: dayjs(element.dateFrom).format('YYYY-MM-DDTHH:mm'),
-    arrivalTimeClass: dayjs(element.dateTo).format('YYYY-MM-DDTHH:mm'),
-    duration: calculateDuration(element.dateFrom, element.dateTo),
-    type: element.type,
-    offers: element.offers,
-    destination: element.destination,
-  }
-));
+const points = getPoints(15);
+const routeInfo = getRouteInfo(points);
 
-const [...cities] = new Set(data.map((element) => element.destination.name));
-const route = cities.join(' — ');
-
-render(appHeaderElement, createTripInfoTemplate(route), 'afterbegin');
-render(appNavigationElement, createAppMenuTemplate(), 'beforeend');
-render(appFiltersElement, createAppFiltersTemplate(), 'beforeend');
-render(appEventsElement, createAppSortTemplate(), 'beforeend');
-render(appEventsElement, createTripEventsTemplate(points), 'beforeend');
+renderTemplate(appHeaderElement, createTripInfoTemplate(routeInfo), 'afterbegin');
+renderTemplate(appNavigationElement, createAppMenuTemplate(), 'beforeend');
+renderTemplate(appFiltersElement, createAppFiltersTemplate(), 'beforeend');
+renderTemplate(appEventsElement, createAppSortTemplate(), 'beforeend');
+renderTemplate(appEventsElement, createTripEventsTemplate(points), 'beforeend');
